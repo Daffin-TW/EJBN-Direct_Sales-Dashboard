ec72a6affe0066a7d84487695a48433c0844dd54
Daffin-TW <dtwdaffintw@gmail.com>
2024-10-23 14:07:00 +0700

    Create a special module for login

---
+++ modules/login.py
@@ -0,0 +1,32 @@
+from time import sleep
+import streamlit as st
+
+
+def login_page():
+    # Create an empty container
+    placeholder = st.empty()
+    state_placeholder = st.empty()
+
+    # Declare username and password
+    actual_username = st.secrets.user_credentials['USERNAME']
+    actual_password = st.secrets.user_credentials['PASSWORD']
+
+    # Insert a form in the container
+    with placeholder.form("login"):
+        st.markdown("#### Enter your credentials")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        submit = st.form_submit_button("Login")
+
+    if submit and username == actual_username and password == actual_password:
+        # clear the form/container and display a success message
+        placeholder.empty()
+        state_placeholder.success("Login successful")
+        sleep(1)
+        st.session_state.login_state = 0
+        state_placeholder.empty()
+
+    elif submit and (username != actual_username or password != actual_password):
+        state_placeholder.error("Login failed")
+    else:
+        pass
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,19 @@
+from modules import login_page
+from time import sleep
 import streamlit as st
 
 
-# Create an empty container
-placeholder = st.empty()
+def initialization():
+    if 'login_state' not in st.session_state:
+        st.session_state.login_state = 0
+    else:
+        pass
 
-# Declare username and password
-actual_username = st.secrets.user_credentials['USERNAME']
-actual_password = st.secrets.user_credentials['PASSWORD']
+def check_login_state():
+    if not st.session_state.login_state:
+        login_page()
+    else:
+        pass
 
-# Insert a form in the container
-with placeholder.form("login"):
-    st.markdown("#### Enter your credentials")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    submit = st.form_submit_button("Login")
 
-if submit and username == actual_username and password == actual_password:
-    # If the form is submitted and the username and password are correct,
-    # clear the form/container and display a success message
-    placeholder.empty()
-    st.success("Login successful")
-elif submit and (username != actual_username or password != actual_password):
-    st.error("Login failed")
-else:
-    pass
-
+initialization()
