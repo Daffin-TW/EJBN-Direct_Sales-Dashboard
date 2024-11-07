088193b4abfcfdfb59662f23892b0916a94b250e
Daffin-TW <dtwdaffintw@gmail.com>
2024-11-07 20:40:26 +0700

    Changes login input stores to session state only

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from streamlit import session_state as ss
+from streamlit import secrets as sc
 import streamlit as st
 
 
-def authentication():
-    actual_username: str = st.secrets.user_credentials.username
-    actual_password: str = st.secrets.user_credentials.password
+def submit_callback():
+    actual_username: str = sc.user_credentials.username
+    actual_password: str = sc.user_credentials.password
+    
+    if ss.username == actual_username and ss.password == actual_password:
+        ss.login_message = True
+        ss.login_state = True
+        
+    elif (ss.username != actual_username or ss.password != actual_password):
+        ss.password = ''
 
+def authentication():
     placeholder = st.empty()
     with placeholder.form('login'):
         st.markdown("#### Enter your credentials")
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        submit = st.form_submit_button("Login")
+        st.text_input("Username", key='username')
+        st.text_input("Password", key='password', type="password")
+        submit = st.form_submit_button("Login", on_click=submit_callback)
 
-    if submit and username == actual_username and password == actual_password:
-        # clear the form/container and display a success message
-        ss.login_message = True
-        ss.login_state = True
+    if submit and ss.login_state:
         placeholder.empty()
-        st.success("Login berhasil")
         st.rerun()
 
-    elif submit and (username != actual_username or password != actual_password):
-        st.error("Login gagal, Username/Password tidak sesuai")
-    else:
-        pass
+    elif submit:
+        st.toast("Login gagal, Username/Password tidak sesuai", icon='⚠')
+
 
 def check_login_state():
     if not ss.get('login_state', False):
+        ss.login_state = False
         authentication()
         if ss.get('login_message', False):
             st.toast('Dimohon untuk login agar dapat mengakses situs...',
